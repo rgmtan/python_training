2a780787b17406dc335a2ff414d8ce5b8fab060c
Ralph Tan <r.tan@klab.com>
2016-03-10 11:47:13 +0800

    added file

---
+++ python/exercises/boilerplate.py
@@ -0,0 +1,11 @@
+# import statements go here
+
+
+# define a main function that displays a greeting
+def main():
+    name = input('What is your name? ')
+    print('Hello, ' + name + '!')
+
+
+if __name__ == '__main__':
+    main()
